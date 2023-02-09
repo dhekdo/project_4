2618b8e1c14ecb0f89b34ca923780abf2b0e7eea
dhekdo <daae0924@naver.com>
2023-02-09 18:59:22 +0900

    2023.02.09

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ window.onload = function(){
     },500);
 }
 
+
+// 썸네일 클릭시 영상 재생
+document.getElementsByClassName("video_click")[0].onclick = function(){
+    this.style.display = "none";
+    // document.getElementsByTagName("iframe")[0].contentWindow.postMessage('{"event" : "command", "func" : "playVideo", "args" : ""}', '*');
+}
+
+
+// 오디오북 클릭시 메뉴 전환
+// document.getElementsByClassName("head_wrap")[0].children().onclick = function(){
+//     this.classList.toggle("audio_on");
+// }
+$(".head_wrap li").click(
+    function(){
+        $(this).addClass("audio_on");
+        $(this).siblings().removeClass("audio_on");
+    }
+);
+
+$(".head_wrap li").click(function(){
+    var headIndex = $(this).index();
+
+    $(".audiomillie > div").eq(headIndex).show().siblings().hide();
+});
+
+// 노란 영상 구좌 영상 클릭시 화면 재생
+document.getElementsByClassName("view_thumbnail")[0].onclick = function(){
+    this.style.display = "none";
+}
+
+
